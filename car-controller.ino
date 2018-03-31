637f1f50ad98ea081039f7aa7156291dcab588fd
mau-guerrero <smg10@rice.edu>
2018-03-31 05:06:17 -0500

    cleans up.

--- car-controller.ino
+++ car-controller.ino
@@ -34,13 +34,13 @@ const double Kd = 0.05;
 // PID setpoint, input, output
 double inputPower; //power of current pedal value
 double setPower; //setpoint of power 
+// rename to l/r
 double current_power_1; //current power of motor
-double current_power_2;
-double newPower;  //power output of PID
+double current_power_2; 
+double newPower;  //power output of PID : do we need another for 2?
 
 
 // Other PID variables
-
 const int MAX_POWER_RATE_THRESHOLD = 500;
 const int KV = 78; //relates rpm and voltage
 
@@ -48,6 +48,7 @@ const int KV = 78; //relates rpm and voltage
 // Hall Effect Sensor variables
 volatile byte rev_count_1;
 volatile byte rev_count_2;
+
 
 unsigned int rpm_1; 
 unsigned int rpm_2; 
@@ -62,14 +63,24 @@ drv sailboat(MOSI, MISO, CLK, SCS, LED);
 PID control_1(&current_power_1, &newPower, &setPower, Kp, Ki, Kd, DIRECT);
 PID control_2(&current_power_2, &newPower, &setPower, Kp, Ki, Kd, DIRECT);
 
-control_1.SetSampleTime(64 * 200);
-control_2.SetSampleTime(64 * 200); //update timings to account for change to Timer 0;
-
-
-
 void setup() {
   Serial.begin(9600); 
+
+  // SPI Pins
+  pinMode(SCS, OUTPUT); pinMode(MOSI, OUTPUT); pinMode(MISO, OUTPUT); pinMode(CLK, OUTPUT); 
   
+  // PWM Pins
+  pinMode(AIN1, OUTPUT); pinMode(AIN2, OUTPUT); pinMode(BIN1, OUTPUT); pinMode(BIN2, OUTPUT); 
+
+  // general DRV pins
+  pinMode(SLEEP, OUTPUT); pinMode(FAULT, INPUT);
+
+  // wake up
+  digitalWrite(SLEEP, HIGH);
+
+  // PWM duty cycle controlled with: 
+  // pin 5-6: OCR0A/B
+  // pin 9-10: OCR1A/B  A = 9 B = 10
   // set PWM Frequency for PWM outputs to 62.5 kHz
   // TIMER/COUNTER 0
   // PIN 6(OC0A) = NON-INVERTED [_BV(COM0A1)]
@@ -87,21 +98,11 @@ void setup() {
   TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10); 
   TCCR1B = _BV(WGM12) | _BV(CS10);
 
-  // SPI Pins
-  pinMode(SCS, OUTPUT); pinMode(MOSI, OUTPUT); pinMode(MISO, OUTPUT); pinMode(CLK, OUTPUT); 
-  
-  // PWM Pins
-  pinMode(AIN1, OUTPUT); pinMode(AIN2, OUTPUT); pinMode(BIN1, OUTPUT); pinMode(BIN2, OUTPUT); 
-
-  // general DRV pins
-  pinMode(SLEEP, OUTPUT); pinMode(FAULT, INPUT);
-
-  // wake up
-  digitalWrite(SLEEP, HIGH);
-
   // DRV registers
-  sailboat.setTBlank(0xC1); // 3us
-  sailboat.setTOff(0x13); // 10us
+  // 3us
+  sailboat.setTBlank(0xC1); 
+  // 10us
+  sailboat.setTOff(0x13); 
   sailboat.setISGain(10);
   sailboat.setTorque(0x8C);
   
@@ -112,99 +113,74 @@ void setup() {
   // IDRIVEN and P (mA)
   sailboat.setIDriveN(100);
   sailboat.setIDriveP(50);
- 
-  // PWM duty cycle controlled with: 
-  // pin 5-6: OCR0A/B
-  // pin 9-10: OCR1A/B  A = 9 B = 10
-  unsigned int read1 = sailboat.read(0x00);
-  Serial.println(read1);
 
-  // pin 9
-  OCR1A = 250;
-  //pin 10 
-  OCR1B = 128;
-  // OCR0A
-  // OCR0B
-
-  // for(OCR1B = 0; OCR1B <= 245; OCR1B++){
-  //   delay(2000);
-  // }
+  //update timings to account for change to Timer 0;
+  // this is a regresh rate of 200ms, definitely too slow. How fast do we go?
+  control_1.SetSampleTime(64 * 200);
+  control_2.SetSampleTime(64 * 200); 
 
   attachInterrupt(1, hall_1_ISR, CHANGE);
   attachInterrupt(0, hall_2_ISR, CHANGE);
+
   rev_count_1 = 0;
   rev_count_2 = 0;
+
   rpm_1 = 0;
   rpm_2 = 0;
+
   time_old_1 = 0;
   time_old_2 = 0;
 
 }
 
-int x;
-int y;
-
-
 void loop() {
+  /* 
 
-  y = map(analogRead(A0), 0, 1023, 0, 255);
-  
-  analogWrite(10, y);
+  RPM determination (millis() func returns 1/64 of millis after Timer 0 manipulation)
 
-
-/* 
-
-RPM determination (millis() func returns 1/64 of millis after Timer 0 manipulation)
-
-        #rotations            5 * (rev_count)
-RPM =  -------------- =  ----------------------------
-        time elapsed      millis() * 64 * 1000 * 60
-*/
-
-if(rev_count_1 >= 5){
-  rpm_1 = (5 * rev_count_1) / (64000 * 60 * (millis() - time_old_1)); 
-  time_old_1 = millis();
-  rev_count_1 = 0;
-}
+            #rotations                     (rev_count)
+  RPM =  -----------------------   =  ----------------------------
+        time elapsed in minutes      (millis() * 64 / 1000) * 60
+  */
+  // average out first 5 revolutions. maybe go for 2 or 3
+  if(rev_count_1 >= 5){
+    // why 5*revs?
+    rpm_1 = (5 * rev_count_1) / (64000 * 60 * (millis() - time_old_1)); 
+    time_old_1 = millis();
+    rev_count_1 = 0;
+  }
 
 
-if(rev_count_2 >= 5){
-  rpm_2 = (5 * rev_count_2) / (64000 * 60 * (millis() - time_old_2));
-  time_old_2 = millis();
-  rev_count_2 = 0;
-}
-
-  // TODO: Check PWM performance
+  if(rev_count_2 >= 5){
+    rpm_2 = (5 * rev_count_2) / (64000 * 60 * (millis() - time_old_2));
+    time_old_2 = millis();
+    rev_count_2 = 0;
+  }
 
   // TODO: calculate setPower based on pedal position AND pedal position rate of change
 
-/*
-
-setPower is:
+  /*
+  setPower is:
   linear map to motor rated power (0 to 1000W) IF rate of depression is not greater than MAX_POWER_RATE_THRESHOLD
-
-
-*/
-
-
-inputPower = map(analogRead(PEDAL), 380, 720, 0, 1000);
-inputPower = constrain(setPower, 0, 1000);
-
-setPower = inputPower;
+  */
+  inputPower = map(analogRead(PEDAL), 380, 720, 0, 1000);
+  inputPower = constrain(setPower, 0, 1000);
+  setPower = inputPower;
 
 
 
   // TODO: caclulcate currentPower based on motors' voltages and currents
 
-current_power_1 = I1 * rpm_1 * KV;
+  current_power_1 = I1 * rpm_1 * KV;
 
-current_power_2 = I2 * rpm_2 * KV;
+  current_power_2 = I2 * rpm_2 * KV;
 
 
   // TODO: run PID.compute()
 
-control_1.compute(); //these will only change value 
-control_2.compute();
+  //these will only change the value of newPower
+  control_1.compute(); 
+  control_2.compute();
 
 
   // TODO: map newPower to pwm signals (need logic for forward, reverse, coasting)
@@ -217,8 +193,8 @@ void hall_left_detect_ISR(){
   Increment hall effect sensor counter on left side
   */
 
-rev_count_1++;
-Serial.println("LEFT 1 RPM");
+  rev_count_1++;
+  Serial.println("LEFT 1 RPM");
 
 }
 
@@ -227,8 +203,8 @@ void hall_right_detect_ISR(){
   Increment hall effect sensor counter on right side 
   */
 
-rev_count_2++;
-Serial.println("RIGHT 1 RPM");
+  rev_count_2++;
+  Serial.println("RIGHT 1 RPM");
 
 }
 
